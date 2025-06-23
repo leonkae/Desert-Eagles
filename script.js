9290958e6ee41b6ebe9b17db13e31ac190874a92
leonkae <leon29kae@gmail.com>
2025-06-23 17:40:24 +0300

    age collection form

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-console.log("Hello, World!");
-// alert("Eddie is awesome!");
+// console.log("Hello, World!");
+// // alert("Eddie is awesome!");
 
-let myName = "Eddie";
-myName = "Eddie the Great" + " the Magnificent";
-console.log(myName);
+// let myName = "Eddie";
+// myName = "Eddie the Great" + " the Magnificent";
+// console.log(myName);
 
-let myAge = 24;
+// let myAge = 24;
 
-console.log(`Andre is ${myAge} years old. and he is ${myName}.`);
-let a  = 5;
-let b = 10;
-function functionName(a,b) {
-    return a + b;
-}
-console.log(`The sum of ${a} and ${b} is ${functionName(a, b)}.`);
-console.log(functionName(a, b));
+// console.log(`Andre is ${myAge} years old. and he is ${myName}.`);
+// let a  = 5;
+// let b = 10;
+// function functionName(a,b) {
+//     return a + b;
+// }
+// console.log(`The sum of ${a} and ${b} is ${functionName(a, b)}.`);
+// console.log(functionName(a, b));
 
-functionName(a,b);
+// functionName(a,b);
+console.log("Alive");
 
 function ticketChecker(age) {
-    if (age < 18) {
-        console.log("You are not allowed to buy a ticket.");
-    }
-}
+  const messageDiv = document.getElementById("message");
 
-ticketChecker(17);
+  if (age < 18) {
+    messageDiv.textContent = "Enda Uoge ulale. !🐶!";
+  } else if (age < 21) {
+    messageDiv.textContent = "Welcome to the party! 🎉 but leave @ 9pm";
+  } else {
+    messageDiv.textContent = "Welcome to the party! 🎉 we know how we do past 9pm ";
+  }
+};
+
+document.getElementById("ageForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent form submission
+  const age = parseInt(document.getElementById("age").value);
+  console.log(`Age entered: ${age}`);
+  ticketChecker(age);
+});
